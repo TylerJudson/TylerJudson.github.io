dd9f0c67b914618cd92f448204d643e8803e99ab
TylerJudson <TylerJudson23@gmail.com>
2021-06-12 06:12:34 -0600

    Added Linting and intelisences

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /* npx tailwindcss-cli@latest build ./css/styles.css -o ./css/tailwind.css */
-
+// https://coolors.co/264653-2a9d8f-e9c46a-f4a261-e76f51
 module.exports = {
   purge: [],
   darkMode: "media", // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'softRed': '#e76f51',
+        'sandy': '#f4a261',
+        'orange': '#e9c46a',
+        'cyan': '#2a9d8f',
+        'teal': '#264653',
+      },
+    },
   },
   variants: {
     extend: {
